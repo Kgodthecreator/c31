48e322ce7ca36a9d2c442cb20b926358617c62f3
Kgodthecreator <57558218+Kgodthecreator@users.noreply.github.com>
2020-01-19 14:49:35 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,26 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smoke = loadImage("sprites/smoke.png");
+    this.trajectory = [];
+
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    super.display();
+    if(this.body.velocity.x>4 && this.body.position.x>200){
+
+    
+    var position = [this.body.position.x,this.body.position.y];
+  
+    this.trajectory.push(position);
+    }
+    for(var num=0;num<this.trajectory.length;num++){
+      image(this.smoke,this.trajectory[num][0],this.trajectory[num][1]);
+    }
+  }
+}
